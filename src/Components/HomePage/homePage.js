20b2abd250172c510e35b103823be03b3c680506
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-11-10 20:57:16 +0530

    Addde Initial Setup

---
+++ src/Components/HomePage/homePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './homePage.scss';
+
+
+class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Entire Graph application will come here</div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.scss';
+import Homepage from './Components/HomePage/homePage';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+
+OfflinePluginRuntime.install();
+
+ReactDOM.render(<Homepage />, document.getElementById('app'));
